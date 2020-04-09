b868f44a2cafe8eda76c66a5a9bdf0196ea419fc
Rustam <rustam.guseynov@thomsonreuters.com>
2020-04-09 18:44:47 +0300

    Start with editors now

--- src/client/app/store/app.state.ts
+++ src/client/app/store/app.state.ts
@@ -8,9 +8,9 @@ export const APP_INITIAL_STATE: IAppState = {
   editors: [
     { path: '/summernote', label: 'Summernote', free: true, selected: false },
     { path: '/joddit', label: 'Joddit', free: true, selected: false },
-    { path: '/quill', label: 'Quill', free: true, selected: false },
-    { path: '/ngx-wig', label: 'Ngx-Wig', free: true, selected: false },
-    { path: '/froala', label: 'Froala', free: false, selected: false },
-    { path: '/ckeditor', label: 'CKEditor', free: false, selected: false },
+    // { path: '/quill', label: 'Quill', free: true, selected: false },
+    // { path: '/ngx-wig', label: 'Ngx-Wig', free: true, selected: false },
+    // { path: '/froala', label: 'Froala', free: false, selected: false },
+    // { path: '/ckeditor', label: 'CKEditor', free: false, selected: false },
   ],
 };
